03f14683337844ac8e498ded33bdf66cbc2fa1a8
YellowDuckcument <109415258+YellowDuckcument@users.noreply.github.com>
2023-02-07 20:43:50 +0700

    modified:   src/pages/MajorEdit.js 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/App.test.js 	modified:   src/components/Input.js 	modified:   src/containers/DefaultLayout.js 	modified:   src/containers/Header.js 	modified:   src/index.css 	modified:   src/index.js 	modified:   src/pages/Home.js 	modified:   src/pages/Login.css 	modified:   src/pages/Login.js 	modified:   src/pages/Major.js 	modified:   src/routes.js
    	src/Student.js
    	src/pages/NotFound.js
    	src/workspace.code-workspace

--- src/pages/MajorEdit.js
+++ src/pages/MajorEdit.js
@@ -1,98 +1,77 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Input from "../components/Input";
 const MajorEdit = () => {
-    return ( <div>
-        <div className="container mt-4">
-            <div className='row justify-content-center'>
-                <div className=''>
-                <div className="card-header">
-                <div className="row">
+  const [major, setMajor] = useState({ id: 0, code: "", name: "" });
+  const { id } = useParams();
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!/\d+/.test(id)) {
+      navigate("/not-found");
+    }
+  }, [id, navigate]);
+
+  const backHandler = (e) => {
+    navigate("/major");
+  };
+
+  const changeEventHandler = (e) => {
+    let newMajor = { ...major };
+    newMajor[e.target.name] = e.target.value;
+    setMajor(newMajor);
+  };
+
+  const saveHandler = () => {
+    console.log(major);
+  };
+
+  return (
+    <div className="container mt-4">
+      <div className="row justify-content-center">
+        <div className="col-md-8 col-lg-6">
+          <div className="card border-primary bt-5">
+            <div className="card-header">
+              <div className="row">
                 <div className="col">
-                    <h3 className="card-title">Major <small className="text-muted">list</small></h3>
+                  <h3 className="card-title">
+                    Major <small className="text-muted">{Number(id) === 0 ? "new" : "update"}</small>
+                  </h3>
                 </div>
-                <div className="col-auto">
-                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal"><i className="bi-plus-lg" /> Add</button>
-                </div>
-                </div>
-      </div>
-                </div>
-            </div>
-    <div className="card border-primary bt-5">
-      
-      <div className="card-body">
-        <div className="table-responsive">
-          <table className="table table-bordered border-primary table-hover table-striped">
-            <thead>
-              <tr className="table-primary border-primary">
-                <th style={{width: 50}}>#</th>
-                <th>Major Name</th>
-                <th style={{width: 80}} />
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>IT</td>
-                <td>
-                  <a href="/#"><i className="bi-pencil-square text-primary" /></a>
-                  <a href="/#"><i className="bi-trash text-danger" /></a>
-                </td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Marketing</td>
-                <td>
-                  <a href="/#"><i className="bi-pencil-square text-primary" /></a>
-                  <a href="/#"><i className="bi-trash text-danger" /></a>
-                </td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>Network</td>
-                <td>
-                  <a href="/#"><i className="bi-pencil-square text-primary" /></a>
-                  <a href="/#"><i className="bi-trash text-danger" /></a>
-                </td>
-              </tr>
-              <tr>
-                <td>4</td>
-                <td>Accounting</td>
-                <td>
-                  <a href="/#"><i className="bi-pencil-square text-primary" /></a>
-                  <a href="/#"><i className="bi-trash text-danger" /></a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  </div>
- {/*  Modal */}
-  <div className="modal fade" id="editModal" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div className="modal-dialog">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h5 className="modal-title" id="exampleModalLabel">New Major</h5>
-          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
-        </div>
-        <div className="modal-body">
-          <form>
-            <div className="row">
-              <label htmlFor="txtMajor" className="col-sm-3 col-form-label required">Major name</label>
-              <div className="col-sm-9">
-                <input type="text" className="form-control" id="txtMajor" />
               </div>
             </div>
-          </form>
-        </div>
-        <div className="modal-footer">
-          <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" className="btn btn-primary">Save</button>
+            <div className="card-body">
+              <Input
+                label="Major code"
+                onChange={changeEventHandler}
+                defaultValue={major.code}
+                name="code"
+                id="txtCode"
+                required
+              />
+              <Input
+                label="Major name"
+                onChange={changeEventHandler}
+                defaultValue={major.name}
+                name="name"
+                id="txtMajor"
+                required
+                lastRow
+              />
+            </div>
+            <div className="card-footer text-center">
+              <button type="button" onClick={backHandler} className="btn btn-secondary me-1">
+                Back
+              </button>
+              <button type="button" onClick={saveHandler} className="btn btn-primary">
+                Save
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-    </div> );
-}
- 
+  );
+};
+
 export default MajorEdit;
