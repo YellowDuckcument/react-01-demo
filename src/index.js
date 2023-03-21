ac2fbbfcdba860861509718fa2895bc41438ae87
Trung Kien <trkien22@gmail.com>
2023-03-21 23:59:13 +0700

    123

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import "bootstrap-icons/font/bootstrap-icons.css";
+// import "bootstrap-icons/font/bootstrap-icons.css";
 
 import "./index.css";
 import App from "./App";
